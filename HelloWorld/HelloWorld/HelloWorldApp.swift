e8732e1b3f10f6a63c1669fc69a419a70e36487b
xiaoyin liu <xliu34@fordham.edu>
2023-04-13 20:56:58 -0400

    init

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Xiaoyin Liu on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
